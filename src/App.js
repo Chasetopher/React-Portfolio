51a7d7bcc1b7c691176dd448e9dce0b768ab73c1
Chasetopher <85038033+Chasetopher@users.noreply.github.com>
2021-10-30 19:16:35 -0400

    added boiler plate code to components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import About from './components/About';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div>
       <Nav />
+      <main>
+        <About />
+        <Projects />
+        <Contact />
+        <Footer />
+      </main>
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <i class="fab fa-linkedin-in"></i>
+            <i class="fab fa-github"></i>
+            <i class="fas fa-at"></i>
+            <i class="far fa-file"></i>
+
+        </div>
+    )
+}
+
+export default Footer;
