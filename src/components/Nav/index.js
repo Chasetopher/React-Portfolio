d5f0482106749df9ccedc234ab213c5ef15588cd
Chasetopher <85038033+Chasetopher@users.noreply.github.com>
2021-10-30 18:23:44 -0400

    added links to navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,7 +4,7 @@ import logo from '../../assets/images/logo.png';
 const Nav = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-            <a className="navbar-brand" href="#"><img src={logo} /> </a>
+            <a className="navbar-brand" href="#home"><img src={logo} alt="logo" /> </a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -12,16 +12,16 @@ const Nav = () => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto">
                     <li className="nav-item active">
-                        <a className="nav-link text-white nav-font" href="#">About</a>
+                        <a className="nav-link text-white nav-font" href="#about">About</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-white nav-font" href="#">Projects</a>
+                        <a className="nav-link text-white nav-font" href="#projects">Projects</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-white nav-font" href="#">Contact</a>
+                        <a className="nav-link text-white nav-font" href="#contact">Contact</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-white nav-font" href="#">Resume</a>
+                        <a className="nav-link text-white nav-font" href="#resume">Resume</a>
                     </li>
                 </ul>
             </div>
