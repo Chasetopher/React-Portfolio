d7417009d6fd25bc5dfa24e81febd64353ad565e
Chasetopher <85038033+Chasetopher@users.noreply.github.com>
2021-10-30 20:11:03 -0400

    added footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,13 +2,18 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            <i class="fab fa-linkedin-in"></i>
-            <i class="fab fa-github"></i>
-            <i class="fas fa-at"></i>
-            <i class="far fa-file"></i>
+        <footer className="fixed-bottom d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div className="col-md-4 d-flex align-items-center">
+      <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <svg className="bi" width="30" height="24"><use xlinkHref="#bootstrap"></use></svg>
+      </a>
+      <span className="text-muted">© 2021 Company, Inc</span>
+    <ul>
+    <img src="/assets/img/bootstrap.svg" alt="Bootstrap" width="32" height="32" />
+    </ul>
+    </div>
+  </footer>
 
-        </div>
     )
 }
 
