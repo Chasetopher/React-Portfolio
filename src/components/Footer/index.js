1b8a841ede951f286b5ed00375f10f7a8f452cd6
Chasetopher <85038033+Chasetopher@users.noreply.github.com>
2021-10-31 11:34:14 -0400

    removed bootstrap components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,20 +10,6 @@ const Footer = () => {
     }, [])
 
     return (
-        <footer className="bg-dark fixed-bottom d-flex flex-wrap justify-content-between align-items-center py-3  border-top">
-    <div className="col-md-4 d-flex align-items-center bg-dark">
-      <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-        <svg className="bi" width="30" height="24"><use xlinkHref="#bootstrap"></use></svg>
-      </a>
-      <span className="text-white">© {date} - Chase Brackeen</span>
-    <ul>
-        <li><img src="../../assets/images/linkedin.svg" alt="Linkedin" width="32" height="32" /></li>
-        <li><img src="../../assets/images/github.svg" alt="Github" width="32" height="32" /></li>
-        <li><img src="../../assets/images/at.svg" alt="Email" width="32" height="32" /></li>
-        <li><img src="../../assets/images/file-earmark-arrow-down.svg" alt="Resume" width="32" height="32" /></li>
-    </ul>
-    </div>
-  </footer>
 
     )
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,31 +3,7 @@ import logo from '../../assets/images/logo.png';
 
 const Nav = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#home"><img src={logo} alt="logo" /> </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link text-white nav-font" href="#about">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-white nav-font" href="#projects">Projects</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-white nav-font" href="#contact">Contact</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-white nav-font" href="#resume">Resume</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        
     )
 }
 
