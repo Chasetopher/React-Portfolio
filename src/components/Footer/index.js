c7b6bd27bf2721b20350d219ed6eefdcdef4b421
Chasetopher <85038033+Chasetopher@users.noreply.github.com>
2021-10-31 10:16:50 -0400

    made the year in footer update on page load

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Footer = () => {
+
+  const [date, setDate] = useState();
+    const getYear = () => setDate(new Date().getFullYear())
+
+    useEffect(() => {
+      getYear();
+    }, [])
+
     return (
-        <footer className="fixed-bottom d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div className="col-md-4 d-flex align-items-center">
+        <footer className="bg-dark fixed-bottom d-flex flex-wrap justify-content-between align-items-center py-3  border-top">
+    <div className="col-md-4 d-flex align-items-center bg-dark">
       <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
         <svg className="bi" width="30" height="24"><use xlinkHref="#bootstrap"></use></svg>
       </a>
-      <span className="text-muted">© 2021 Company, Inc</span>
+      <span className="text-white">© {date} - Chase Brackeen</span>
     <ul>
         <li><img src="../../assets/images/linkedin.svg" alt="Linkedin" width="32" height="32" /></li>
         <li><img src="../../assets/images/github.svg" alt="Github" width="32" height="32" /></li>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,31 +3,32 @@ import logo from '../../assets/images/logo.png';
 
 const Nav = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-            <a className="navbar-brand" href="#home"><img src={logo} alt="logo" /> </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#home"><img src={logo} alt="logo" /> </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ms-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link text-white nav-font" href="#about">About</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link text-white nav-font" href="#projects">Projects</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link text-white nav-font" href="#contact">Contact</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link text-white nav-font" href="#resume">Resume</a>
-                    </li>
-                </ul>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link text-white nav-font" href="#about">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-white nav-font" href="#projects">Projects</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-white nav-font" href="#contact">Contact</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-white nav-font" href="#resume">Resume</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
 }
 
 export default Nav;
-
