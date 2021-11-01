da3f9d1f8493528908e78b68badedb266f6a6ed1
Chasetopher <85038033+Chasetopher@users.noreply.github.com>
2021-10-31 22:37:19 -0400

    updated navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -9,11 +9,12 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 import Button from '@mui/material/Button';
-
 //MATERIAL UI END//
 
-export default function MenuAppBar() {
+const Nav = () => {
   
   const [anchorEl, setAnchorEl] = React.useState(null);
 
@@ -25,45 +26,61 @@ export default function MenuAppBar() {
     setAnchorEl(null);
   };
 
+  //set breakpoints more mobile responsiveness
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar color="secondary" position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Photos
+            <img src={logo} alt="logo" />
           </Typography>
-            <div>
-              <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-                onClick={handleMenu}
-              >
+            <div>              
+              { isMobile ? (
+                //IF SMALL SCREEN, HAMBURGER MENU
+                <>
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  sx={{ mr: 2 }}
+                  onClick={handleMenu}
+                >
                 <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Home</MenuItem>
-                <MenuItem onClick={handleClose}>About</MenuItem>
-                <MenuItem onClick={handleClose}>Projects</MenuItem>
-                <MenuItem onClick={handleClose}>Contact</MenuItem>
-                <MenuItem onClick={handleClose}>Resume</MenuItem>
-              </Menu>
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>About</MenuItem>
+                  <MenuItem onClick={handleClose}>Projects</MenuItem>
+                  <MenuItem onClick={handleClose}>Contact</MenuItem>
+                  <MenuItem onClick={handleClose}>Resume</MenuItem>
+                </Menu>
+                </>
+                ) : (
+                  //ELSE, USE BUTTONS
+                  <>
+                  <Button color="inherit">About</Button>
+                  <Button color="inherit">Projects</Button>
+                  <Button color="inherit">Contact</Button>
+                  <Button color="inherit">Resume</Button>
+                  </>
+              )}
             </div>
         </Toolbar>
       </AppBar>
@@ -71,138 +88,4 @@ export default function MenuAppBar() {
   );
 }
 
-
-/* <Button color="inherit">About</Button>
-<Button color="inherit">Projects</Button>
-<Button color="inherit">Contact</Button>
-<Button color="inherit">Resume</Button> */
-
-
-
-
-// import AppBar from '@mui/material/AppBar';
-// import Box from '@mui/material/Box';
-// import Toolbar from '@mui/material/Toolbar';
-// import IconButton from '@mui/material/IconButton';
-// import Typography from '@mui/material/Typography';
-// import Badge from '@mui/material/Badge';
-// import MenuItem from '@mui/material/MenuItem';
-// import Menu from '@mui/material/Menu';
-// import MailIcon from '@mui/icons-material/Mail';
-// import NotificationsIcon from '@mui/icons-material/Notifications';
-// import MoreIcon from '@mui/icons-material/MoreVert';
-
-// const Nav = () => {
-
-//   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-//   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-//   const handleMobileMenuClose = () => {
-//     setMobileMoreAnchorEl(null);
-//   };
-
-//   const handleMobileMenuOpen = (event) => {
-//     setMobileMoreAnchorEl(event.currentTarget);
-//   };
-
-//   const menuId = 'primary-search-account-menu';
-//   const renderMenu = (
-//     <Menu
-//       id={menuId}
-//       keepMounted
-//       transformOrigin={{
-//         vertical: 'top',
-//         horizontal: 'right',
-//       }}
-//     >
-//     </Menu>
-//   );
-
-//   const mobileMenuId = 'primary-search-account-menu-mobile';
-//   const renderMobileMenu = (
-//     <Menu
-//       anchorEl={mobileMoreAnchorEl}
-//       anchorOrigin={{
-//         vertical: 'top',
-//         horizontal: 'right',
-//       }}
-//       id={mobileMenuId}
-//       keepMounted
-//       transformOrigin={{
-//         vertical: 'top',
-//         horizontal: 'right',
-//       }}
-//       open={isMobileMenuOpen}
-//       onClose={handleMobileMenuClose}
-//     >
-//       <MenuItem>
-//         <IconButton size="large" color="inherit">
-          
-//             <MailIcon />
-          
-//         </IconButton>
-//         <p>Messages</p>
-//       </MenuItem>
-//       <MenuItem>
-//         <IconButton
-//           size="large"
-//           color="inherit"
-//         >
-//           <Badge>
-//             <NotificationsIcon />
-//           </Badge>
-//         </IconButton>
-//         <p>Notifications</p>
-//       </MenuItem>
-//     </Menu>
-//   );
-
-//     return (
-//         <Box sx={{ flexGrow: 1 }}>
-//         <AppBar position="sticky">
-//           <Toolbar>
-//             <Typography
-//               variant="h6"
-//               noWrap
-//               component="div"
-//               sx={{ display: { xs: 'none', sm: 'block' } }}
-//             >
-//               {logo}
-//             </Typography>
-//             <Box sx={{ flexGrow: 1 }} />
-//             <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-//               <IconButton size="large" color="inherit">
-//                 <Badge>
-//                   <MailIcon />
-//                 </Badge>
-//               </IconButton>
-//               <IconButton
-//                 size="large"
-//                 color="inherit"
-//               >
-//                 <Badge>
-//                   <NotificationsIcon />
-//                 </Badge>
-//               </IconButton>
-//             </Box>
-//             <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-//               <IconButton
-//                 size="large"
-//                 aria-label="show more"
-//                 aria-controls={mobileMenuId}
-//                 aria-haspopup="true"
-//                 onClick={handleMobileMenuOpen}
-//                 color="inherit"
-//               >
-//                 <MoreIcon />
-//               </IconButton>
-//             </Box>
-//           </Toolbar>
-//         </AppBar>
-//         {renderMobileMenu}
-//         {renderMenu}
-//       </Box>
-//     )
-// }
-// export default Nav;
+export default Nav;
