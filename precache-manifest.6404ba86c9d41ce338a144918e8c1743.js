24a9d72b30519f0dcaeeba28414b61841b6a1c50
Chasetopher <85038033+Chasetopher@users.noreply.github.com>
2021-11-06 13:15:20 -0400

    Updates

---
+++ precache-manifest.6404ba86c9d41ce338a144918e8c1743.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cecf9db4d6c4d97fd23836f473d4a4a2",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "12420bbeacb2b213811f",
+    "url": "/React-Portfolio/static/css/2.e3c5ea6a.chunk.css"
+  },
+  {
+    "revision": "8a5b89d06b313eeb5974",
+    "url": "/React-Portfolio/static/css/main.def81194.chunk.css"
+  },
+  {
+    "revision": "12420bbeacb2b213811f",
+    "url": "/React-Portfolio/static/js/2.44432887.chunk.js"
+  },
+  {
+    "revision": "bfc3e7e0a97125301e8132b6d78b9d38",
+    "url": "/React-Portfolio/static/js/2.44432887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5b89d06b313eeb5974",
+    "url": "/React-Portfolio/static/js/main.0c3ff3c6.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "73e7acc08f4fc7f17a2a4c1baae72dfb",
+    "url": "/React-Portfolio/static/media/Resume.73e7acc0.pdf"
+  },
+  {
+    "revision": "d55559f9836b389ddb0d304dc9d494e3",
+    "url": "/React-Portfolio/static/media/chillcover.d55559f9.gif"
+  },
+  {
+    "revision": "1a62f875d810d81ceb885f595e5db1a9",
+    "url": "/React-Portfolio/static/media/deep-blue.1a62f875.png"
+  },
+  {
+    "revision": "b1fe04d072796d0baa233d1ee9129a2e",
+    "url": "/React-Portfolio/static/media/deep-thoughts.b1fe04d0.png"
+  },
+  {
+    "revision": "f7a2c73711edbbd6f273a13766491de1",
+    "url": "/React-Portfolio/static/media/kiona-itallic-webfont.f7a2c737.woff"
+  },
+  {
+    "revision": "4ff742999f14b4387f06adbb609d4f82",
+    "url": "/React-Portfolio/static/media/kiona-regular-webfont.4ff74299.woff"
+  },
+  {
+    "revision": "3bfbb3e62764d27cfe3c4948b7e44299",
+    "url": "/React-Portfolio/static/media/personal-photo.3bfbb3e6.jpeg"
+  },
+  {
+    "revision": "27245fcb333ca2efaeac66fbfc057dc8",
+    "url": "/React-Portfolio/static/media/pizza-hunt.27245fcb.png"
+  },
+  {
+    "revision": "f4a4ecaa4c2c787becf58e116b08417d",
+    "url": "/React-Portfolio/static/media/readme-generator.f4a4ecaa.png"
+  },
+  {
+    "revision": "130eafc23a987a6cf560c9b69af84818",
+    "url": "/React-Portfolio/static/media/roboto-all-300-normal.130eafc2.woff"
+  },
+  {
+    "revision": "73f26bf98a715ecab4d2287ff3a02ad0",
+    "url": "/React-Portfolio/static/media/roboto-all-400-normal.73f26bf9.woff"
+  },
+  {
+    "revision": "08926d7a008503f9c640b1772c225476",
+    "url": "/React-Portfolio/static/media/roboto-all-500-normal.08926d7a.woff"
+  },
+  {
+    "revision": "8b2b2aae46819bb8c37c438760dbb4f6",
+    "url": "/React-Portfolio/static/media/roboto-all-700-normal.8b2b2aae.woff"
+  },
+  {
+    "revision": "4a2f6d1316cc560ede07d3334d3b486a",
+    "url": "/React-Portfolio/static/media/roboto-cyrillic-300-normal.4a2f6d13.woff2"
+  },
+  {
+    "revision": "ba2c6cb0af81f8da49a960db84f26b7c",
+    "url": "/React-Portfolio/static/media/roboto-cyrillic-400-normal.ba2c6cb0.woff2"
+  },
+  {
+    "revision": "ad72d5d4f30e8740d5e7aa4ba3348aa4",
+    "url": "/React-Portfolio/static/media/roboto-cyrillic-500-normal.ad72d5d4.woff2"
+  },
+  {
+    "revision": "37afd1fecbffb80a9eded06f4bb964b8",
+    "url": "/React-Portfolio/static/media/roboto-cyrillic-700-normal.37afd1fe.woff2"
+  },
+  {
+    "revision": "607808ee335a962bdfa989bbfd5e8c57",
+    "url": "/React-Portfolio/static/media/roboto-cyrillic-ext-300-normal.607808ee.woff2"
+  },
+  {
+    "revision": "2e0b8660b87034ecf56aa9f488fbc08e",
+    "url": "/React-Portfolio/static/media/roboto-cyrillic-ext-400-normal.2e0b8660.woff2"
+  },
+  {
+    "revision": "d697abd346b57baaaa3c64733e998c6a",
+    "url": "/React-Portfolio/static/media/roboto-cyrillic-ext-500-normal.d697abd3.woff2"
+  },
+  {
+    "revision": "638fd23bbc8523124320a4bee32cc43b",
+    "url": "/React-Portfolio/static/media/roboto-cyrillic-ext-700-normal.638fd23b.woff2"
+  },
+  {
+    "revision": "203e97b336d3bc83c8b492a9868d5574",
+    "url": "/React-Portfolio/static/media/roboto-greek-300-normal.203e97b3.woff2"
+  },
+  {
+    "revision": "22786f243202d7912399ffd10c76fe78",
+    "url": "/React-Portfolio/static/media/roboto-greek-400-normal.22786f24.woff2"
+  },
+  {
+    "revision": "89de9101b10a3fd497fef480319ff743",
+    "url": "/React-Portfolio/static/media/roboto-greek-500-normal.89de9101.woff2"
+  },
+  {
+    "revision": "52df702db98aa7138730d8ef06b3a71c",
+    "url": "/React-Portfolio/static/media/roboto-greek-700-normal.52df702d.woff2"
+  },
+  {
+    "revision": "853ac2aeeb42298d3b87f80fdf1f9486",
+    "url": "/React-Portfolio/static/media/roboto-greek-ext-300-normal.853ac2ae.woff2"
+  },
+  {
+    "revision": "5cff07beab63ec777fc73ac0483811b0",
+    "url": "/React-Portfolio/static/media/roboto-greek-ext-400-normal.5cff07be.woff2"
+  },
+  {
+    "revision": "643470710a60fdc4a1c3df732b114ef5",
+    "url": "/React-Portfolio/static/media/roboto-greek-ext-500-normal.64347071.woff2"
+  },
+  {
+    "revision": "1a7d7a36c39d76fb49a80f1b51baf065",
+    "url": "/React-Portfolio/static/media/roboto-greek-ext-700-normal.1a7d7a36.woff2"
+  },
+  {
+    "revision": "80fe119e5efa3911b9d61b265f723b3d",
+    "url": "/React-Portfolio/static/media/roboto-latin-300-normal.80fe119e.woff2"
+  },
+  {
+    "revision": "aa23b7b4bcf2b8f0e876106bb3de69c6",
+    "url": "/React-Portfolio/static/media/roboto-latin-400-normal.aa23b7b4.woff2"
+  },
+  {
+    "revision": "f00e7e4432f7c70d8c97efbe2c50d43b",
+    "url": "/React-Portfolio/static/media/roboto-latin-500-normal.f00e7e44.woff2"
+  },
+  {
+    "revision": "bf28241e67511184c14dbd0ef7d39f91",
+    "url": "/React-Portfolio/static/media/roboto-latin-700-normal.bf28241e.woff2"
+  },
+  {
+    "revision": "065438c98de2b7f979decf5d7e3eb8a0",
+    "url": "/React-Portfolio/static/media/roboto-latin-ext-300-normal.065438c9.woff2"
+  },
+  {
+    "revision": "718dded3393324e992b225ac61329e0c",
+    "url": "/React-Portfolio/static/media/roboto-latin-ext-400-normal.718dded3.woff2"
+  },
+  {
+    "revision": "dd464b28ac4bc3d57d9336cf31fde8f1",
+    "url": "/React-Portfolio/static/media/roboto-latin-ext-500-normal.dd464b28.woff2"
+  },
+  {
+    "revision": "01a68cca6394bb55312ae1d723285d73",
+    "url": "/React-Portfolio/static/media/roboto-latin-ext-700-normal.01a68cca.woff2"
+  },
+  {
+    "revision": "d47048a60dc372aa9776bb0b816bb238",
+    "url": "/React-Portfolio/static/media/roboto-vietnamese-300-normal.d47048a6.woff2"
+  },
+  {
+    "revision": "756af8e5560d200e53b52b1ff70f2ad0",
+    "url": "/React-Portfolio/static/media/roboto-vietnamese-400-normal.756af8e5.woff2"
+  },
+  {
+    "revision": "b32ad0e3f979400d3653dd13459777ae",
+    "url": "/React-Portfolio/static/media/roboto-vietnamese-500-normal.b32ad0e3.woff2"
+  },
+  {
+    "revision": "371c52ef9af49885977af6fbffe823f9",
+    "url": "/React-Portfolio/static/media/roboto-vietnamese-700-normal.371c52ef.woff2"
+  },
+  {
+    "revision": "eeeb6fd10384f2ff90e9b2c4665f4297",
+    "url": "/React-Portfolio/static/media/work-scheduler.eeeb6fd1.png"
+  }
+]);
